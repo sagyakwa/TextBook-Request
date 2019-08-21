30629df0d630d29efe63cb9058da906e951256a3
Samuel Agyakwa <samuelagyen@gmail.com>
2019-08-20 20:12:59 -0400

    changed focus property of some widgets

--- BookRequest.py
+++ BookRequest.py
@@ -3,16 +3,21 @@ import ssl
 from os.path import join, dirname, abspath
 
 import yaml
-
 from flask import Flask, render_template, request
+
+import libgen
 
 credential_location = join(dirname(abspath(__file__)), 'credentials')
 credentials = yaml.load(open(credential_location))
 
+libgenio = 'http://93.174.95.29'
+libgenme = 'http://libgen.me'
+booksdl = 'http://libgen.io/get'
+
 port = 465
 from_email = credentials['credentials']['from_email']
 password = credentials['credentials']['password']
-to_email = credentials['credentials']['to_email']
+# to_email = credentials['credentials']['to_email']
 
 context = ssl.create_default_context()
 
@@ -21,27 +26,34 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    global name, email, textbook
+    global name, to_email, textbook
     name = request.form.get('username')
-    email = request.form.get('email')
+    to_email = request.form.get('email')
     textbook = request.form.get('textbook')
     return render_template('index.html')
 
 
 @app.route('/', methods=['POST'])
 def form_post():
-    global name, email, textbook, author, ISBN
+    global name, to_email, ISBN
     name = request.form.get('name')
-    email = request.form.get('email')
-    textbook = request.form.get('textbook')
-    author = request.form.get('author')
+    to_email = request.form.get('email')
     ISBN = request.form.get('ISBN')
 
-    msg = f'{name}\n{email}\n{textbook}\n{author}\n{ISBN}\n\n'
-    subject = "New Text Book Request"
+    get = libgen.Get(ISBN)
+    link_mirror_1 = get.link_from()
+    link_mirror_2 = get.link_from(libgenme, 'Get from vault')
+    link_mirror_3 = get.link_from(booksdl)
+    links = link_mirror_1 + link_mirror_2 + link_mirror_3
+    new_list = '\n\n'.join('{}'.format(item) for item in links)
+
+    msg = f'Hey {name}, \nHere are the links for download\n{new_list}'
+    subject = 'Textbook Links'
     info = 'Subject: {}\n\n{}'.format(subject, msg)
 
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        print(info)
+        print(to_email)
         server.login(from_email, password)
         server.sendmail(from_email, to_email, info)
         server.quit()
